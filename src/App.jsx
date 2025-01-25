6793bf29dc921ebb77b3ae03a31e8d7d3652216d
pikachu <msram2006@gmail.com>
2025-01-26 00:30:52 +0530

    integrated api request for latest movie and added styling for all the pages using css.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import { Routes, Route } from "react-router-dom"
 import Favorites from './pages/Favorites';
 import NavBar from './components/NavBar';
-
+import "./css/App.css"
 function App() {
 
   return (
